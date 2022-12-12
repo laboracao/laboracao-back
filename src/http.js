0615549e5d31ede004bb01792fd8f59d749a6093
rafael.estevamdesign@gmail.com <rafael.estevamdesign@gmail.com>
2022-12-12 14:50:02 -0300

    Env

--- src/http.js
+++ src/http.js
@@ -15,7 +15,7 @@ const io = socketIo(server, {
   }
 });
 
-mongoose.connect('mongodb+srv://laboracao:VvvxrH1ZecXEEpUq@laboracao.o1i28.mongodb.net/laboracao?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
