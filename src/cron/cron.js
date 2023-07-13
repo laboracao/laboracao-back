4167f96b599b07a5b66264d6baf1641e111c56fd
rafael.estevamdesign@gmail.com <rafael.estevamdesign@gmail.com>
2023-07-13 00:06:15 -0300

    Cron versel

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const connectMongo = async () => {
-  const mongo = await mongoose.createConnection(process.env.MONGODB_URI);
+  const mongo = mongoose.createConnection(process.env.MONGODB_URI);
   const dropCollection = await mongo.dropCollection('gamifications');
   console.log('Gamifications Droped', dropCollection);
 }
 
-export async function executeClearGamification(){
-  await connectMongo();
+export function executeClearGamification(){
+  connectMongo();
 }
