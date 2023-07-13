5a42f7ffc57552ea81199e826fca256ff5306962
rafael.estevamdesign@gmail.com <rafael.estevamdesign@gmail.com>
2023-07-12 23:11:04 -0300

    Cron functions

--- src/controller/cronController.js
+++ src/controller/cronController.js
@@ -2,7 +2,8 @@ var cron = require('node-cron');
 
 module.exports = {
   async executeClearGamification(mongoose){
-    cron.schedule('0 0 1 1-12 *', async () => {
+    // cron.schedule('0 0 1 1-12 *', async () => {
+    cron.schedule('* * * * *', async () => {
       console.log('Running...');
       const mongo = await mongoose.createConnection(process.env.MONGODB_URI);
       const dropCollection = await mongo.dropCollection('gamifications');
