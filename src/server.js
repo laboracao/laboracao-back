d3ec6c9b2ee183dc75bd5edc4b3391c74f7b56b3
rafael.estevamdesign@gmail.com <rafael.estevamdesign@gmail.com>
2023-07-13 17:19:51 -0300

    Versel Cron functions

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const { server, mongoose } = require('./http');
 const userWebsocket = require('./websocket/users');
-const cron = require('./controller/cronController');
+// const cron = require('./controller/cronController');
 
 const porta = process.env.PORT || 8081;
 
-cron.executeClearGamification(mongoose);
+// cron.executeClearGamification(mongoose);
 
 server.listen(porta);
