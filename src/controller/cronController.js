5d0218cac0cd03e52e2a958e59a5b600f5a7f876
rafael.estevamdesign@gmail.com <rafael.estevamdesign@gmail.com>
2023-07-12 23:40:42 -0300

    Cron functions

--- src/controller/cronController.js
+++ src/controller/cronController.js
@@ -2,12 +2,37 @@ var cron = require('node-cron');
 
 module.exports = {
   async executeClearGamification(mongoose){
+
+    // const mongo = await mongoose.createConnection(process.env.MONGODB_URI);
+    // const gamificationCollections = mongoose.connection.collections["gamifications"];
+
+    // if(!gamificationCollections){
+    //   return;
+    // }
+
+    // const test = await mongoose.connection.collections["gamifications"].drop()
+
+    // console.log(test);
+
     // cron.schedule('0 0 1 1-12 *', async () => {
     cron.schedule('* * * * *', async () => {
-      console.log('Running...');
-      const mongo = await mongoose.createConnection(process.env.MONGODB_URI);
-      const dropCollection = await mongo.dropCollection('gamifications');
-      console.log('Gamifications Droped', dropCollection)
+      // console.log('Running...');
+      // const mongo = await mongoose.createConnection(process.env.MONGODB_URI);
+      // const hasGamificationCollection = await mongo.getDatabases('gamifications')
+
+      // console.log(hasGamificationCollection);
+
+      // const dropCollection = await mongo.dropCollection('gamifications');
+      // console.log('Gamifications Droped', dropCollection)
+
+      const gamificationCollections = mongoose.connection.collections["gamifications"];
+
+      if(!gamificationCollections){
+        return;
+      }
+
+      const gamificationDrop = await mongoose.connection.collections["gamifications"].drop();
+      console.log(gamificationDrop);
     }, {
       scheduled: true,
       timezone: "America/Sao_Paulo"
